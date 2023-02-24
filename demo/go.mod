0e8b577415f86bf70f2231e529b6081b844b7e21
Abhinav Gupta <mail@abhinavg.net>
2023-02-23 19:50:02 -0800

    Add demo website (#3)
    Adds a demo website to the documentation.
    The demo website uses WASM to allow previewing the plugin output
    on a static web page.

---
+++ demo/go.mod
@@ -0,0 +1,10 @@
+module go.abhg.dev/goldmark/anchor/demo
+
+go 1.20
+
+replace go.abhg.dev/goldmark/anchor => ../
+
+require (
+	github.com/yuin/goldmark v1.5.3
+	go.abhg.dev/goldmark/anchor v0.1.0
+)
---
+++ demo/main.go
@@ -0,0 +1,48 @@
+// demo implements a WASM module that can be used to format markdown
+// with the goldmark-anchor extension.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"syscall/js"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/parser"
+	"go.abhg.dev/goldmark/anchor"
+)
+
+func main() {
+	js.Global().Set("formatMarkdown", js.FuncOf(formatMarkdown))
+	select {}
+}
+
+func formatMarkdown(this js.Value, args []js.Value) any {
+	input := args[0].Get("markdown").String()
+	var pos anchor.Position
+	switch s := args[0].Get("position").String(); s {
+	case "before":
+		pos = anchor.Before
+	case "after":
+		pos = anchor.After
+	default:
+		return fmt.Sprintf("invalid position: %q", s)
+	}
+
+	md := goldmark.New(
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithExtensions(
+			&anchor.Extender{
+				Position: pos,
+			},
+		),
+	)
+
+	var buf bytes.Buffer
+	if err := md.Convert([]byte(input), &buf); err != nil {
+		return err.Error()
+	}
+	return buf.String()
+}
